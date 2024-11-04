91e42a638aa3d459126ed42f4b93ec9ca972531a
Bidhan Roy <bidhanroy578@gmail.com>
2024-11-04 23:56:36 +0600

    nav and hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Hero from './Components/Hero/Hero'
+import Navbar from './Components/Navbar/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl '>Vite + React</h1>
-      
-    </>
+    <div className='max-w-7xl mx-auto'>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </div>
   )
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { CiSearch  } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+const Navbar = () => {
+    return (
+        <div className='m-10' >
+            <div className='grid grid-cols-3 grid-flow-row gap-10 items-center'>
+                <div className='text-3xl lexend-bold'>Recipe Calories</div>
+                <div className='flex justify-between'>
+                    <a href="">Home</a>
+                    <a href="">Recipes</a>
+                    <a href="">About</a>
+                    <a href="">Search</a>
+                </div>
+                <div className='flex justify-between'>
+                    <div className='relative flex items-center'>
+                        <input type="text" placeholder='Search' className='absolute pl-10 text-[#150B2BB3] text-base lexend-regular box-border p-2 rounded-3xl bg-[#150B2B0D]'/>
+                        <CiSearch className='text-3xl text-[#150B2B80] pl-1' />
+                    </div>
+                    <div>
+                        <div>
+                        <CgProfile className='bg-[#0BE58A] text-4xl rounded-full p-1 hover:text-3xl'/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar; <h3>nav here </h3>
