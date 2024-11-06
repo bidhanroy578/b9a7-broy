de4124fcca7df191dcf7eca04ce647a3589f338e
Bidhan Roy <bidhanroy578@gmail.com>
2024-11-06 14:30:29 +0600

    nav bar responsive and font correction done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,15 +3,15 @@ import { CgProfile } from "react-icons/cg";
 const Navbar = () => {
     return (
         <div className='m-2 md:mx-4 md:my-14' >
-            <div className='grid md:grid-cols-3 grid-flow-row gap-10 items-center'>
+            <div className='grid md:grid-cols-2 gap-2 lg:grid-cols-3 items-center '>
                 <div className='text-3xl lexend-bold'>Recipe Calories</div>
-                <div className='flex justify-between text-[#150B2BB3] lexend-regular text-base'>
+                <div className='flex justify-between sm:p-10 p-2 lg:w-[80%] md:hidden lg:flex  text-[#150B2BB3] lexend-regular text-base'>
                     <a href="">Home</a>
                     <a href="">Recipes</a>
                     <a href="">About</a>
                     <a href="">Search</a>
                 </div>
-                <div className='flex justify-between w-[90%]'>
+                <div className='flex justify-between '>
                     <div className='relative flex items-center'>
                         <input type="text" placeholder='Search' className='absolute pl-10 text-[#150B2BB3] text-base lexend-regular box-border p-2 rounded-3xl bg-[#150B2B0D]'/>
                         <CiSearch className='text-3xl text-[#150B2B80] pl-1' />
